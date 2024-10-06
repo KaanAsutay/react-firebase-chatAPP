f67691bf56c37319c0a29a28fc6119454de59c06
Kaan <kaanasutay10@gmail.com>
2024-10-06 16:12:59 +0300

    React Search Bar Design

--- src/components/list/chatList/chatList.jsx
+++ src/components/list/chatList/chatList.jsx
@@ -1,8 +1,30 @@
+import { useState } from "react";
 import "./chatList.css"
 
 const ChatList = () => {
+    const [addMode, setAddMode] = useState(false)
+
+
     return (
-        <div className="chatList">ChatList</div>
+        <div className="chatList">
+
+            <div className="search">
+                <div className="searchBar">
+                    <img src="/search.png" />
+                    <input 
+                    type="text" 
+                    placeholder="Search..." 
+                    />
+                </div>
+
+                <img 
+                src={addMode ? "./minus.png" : "./plus.png"} 
+                className="add" 
+                onClick={() => setAddMode((prev) => !prev)} 
+                />
+
+            </div>
+        </div>
     )
 }
 
