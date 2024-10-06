f84e48e3b08544aa4e89658edf0552559e8b5fd6
Kaan <kaanasutay10@gmail.com>
2024-10-06 16:28:20 +0300

    React Chat Component Design

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -2,7 +2,30 @@ import "./chat.css"
 
 const Chat = () => {
     return (
-        <div className="chat">Chat</div>
+        <div className="chat">
+
+            <div className="top">
+
+                <div className="user">
+                    <img src="./avatar.png" />
+                    <div className="texts">
+                        <span>Jane Doe</span>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+                </div>
+                
+                <div className="icons">
+                    <img src="./phone.png" />
+                    <img src="./video.png" />
+                    <img src="./info.png" />
+                </div>
+            </div>
+            
+            <div className="center"></div>
+
+            <div className="bottom"></div>
+
+        </div>
     )
 }
 
