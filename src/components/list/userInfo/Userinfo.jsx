1dbb0911fb3bfdf1c103b807564b4dae7802cf6d
Kaan <kaanasutay10@gmail.com>
2024-10-06 15:37:10 +0300

    Chat List Design

---
+++ src/components/list/userInfo/Userinfo.jsx
@@ -0,0 +1,24 @@
+import "./userInfo.css"
+
+const Userinfo = () => {
+    return (
+        <div className="userInfo">
+
+            <div className="user">
+            <img src="./avatar.png" />
+            <h2>John Doe</h2>
+            </div>
+
+            <div className="icons">
+
+            <img src="./more.png" />
+            <img src="./video.png" />
+            <img src="./edit.png" />
+
+            </div>
+            
+        </div>
+    )
+}
+
+export default Userinfo;
